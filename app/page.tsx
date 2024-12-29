f4d1b5be0329bf148f603696de5996ebd3442d6e
DanielVNZ <123446671+DanielVNZ@users.noreply.github.com>
2024-12-30 10:50:08 +1300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -258,7 +258,7 @@ export default function Chat() {
           rel="noopener noreferrer"
           className="absolute bottom-5 right-5 bg-gray-200 dark:bg-gray-700 text-black dark:text-white rounded-full p-2 shadow-md transition hover:bg-gray-300 dark:hover:bg-gray-600"
         >
-          💲Donate Here💲
+          💲💗 Donate Here 
         </a>
       </div>
     </main>
