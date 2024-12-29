689d388073b92072d90c58c71139838814ce01c4
DanielVNZ <123446671+DanielVNZ@users.noreply.github.com>
2024-12-29 19:26:23 +1300

    changed to gpt4o

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -61,7 +61,7 @@ export async function POST(req: Request) {
       const result = await runFunction(name, args);
       const newMessages = createFunctionCallMessages(result);
       return openai.chat.completions.create({
-        model: "gpt-3.5-turbo-0613",
+        model: "gpt-4o",
         stream: true,
         messages: [...messages, ...newMessages],
       });
