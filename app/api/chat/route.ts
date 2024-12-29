e60c92cd548a53d23046e16c4e787531010551de
DanielVNZ <123446671+DanielVNZ@users.noreply.github.com>
2024-12-29 19:20:32 +1300

    changed GPT to gpt-40

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -46,7 +46,7 @@ export async function POST(req: Request) {
 
   // check if the conversation requires a function call to be made
   const initialResponse = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo-0613",
+    model: "gpt-4o",
     messages,
     stream: true,
     functions,
