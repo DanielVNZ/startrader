93106eeb949aae497af92002c076149013a3ab97
DanielVNZ <123446671+DanielVNZ@users.noreply.github.com>
2024-12-29 23:13:52 +1300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,6 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class", // Enable class-based dark mode
   plugins: [require("@tailwindcss/typography")],
 };
